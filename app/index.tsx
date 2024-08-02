613637064d6aaed0355bd7926c581ceb046e0a6a
David AfterDark <david.notjustdev@gmail.com>
2024-08-01 22:13:23 -0400

    feat: add AnimatedIntro

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View } from 'react-native'
+import AnimatedIntro from '@/components/AnimatedIntro'
+
+export default function Index() {
+  return (
+    <View style={{ flex: 1 }}>
+      <AnimatedIntro />
+    </View>
+  )
+}
