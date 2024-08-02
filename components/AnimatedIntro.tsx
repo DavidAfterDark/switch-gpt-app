9f9294b0e3812cc9a6c9f432fc7a19972951ecfe
David AfterDark <david.notjustdev@gmail.com>
2024-08-01 22:10:16 -0400

    chore: remove sample files

---
+++ components/AnimatedIntro.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function AnimatedIntro() {
+  return (
+    <View>
+      <Text>AnimatedIntro</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
