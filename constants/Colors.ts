e52fe10f387825c7424ff4a33a53e4ae23e2c2f2
David AfterDark <david.notjustdev@gmail.com>
2024-08-04 17:41:53 -0400

    style: set colors palettle

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,25 +1,51 @@
-const tintColorLight = '#0a7ea4'
-const tintColorDark = '#fff'
+const ColorsPalette = {
+  paleBlue100: '#3d3f40',
+  paleBlue200: '#13343b',
+  paleCyan100: '#8d9191',
+  paleCyan200: '#2d2f2f',
+  paleCyan300: '#202222',
+  paleCyan400: '#191a1a',
+  paleTeal100: '#dfe7e4',
+  paleYellow100: '#fcfcf9',
+  paleYellow200: '#f3f3ee',
+  paleYellow300: '#e8e8e3',
+  paleYellow400: '#e8e8e6',
+  paleYellow500: '#e0e0dc',
+  paleYellow600: '#d9d9d0',
+  paleYellow700: '#64645f',
+  paleYellow800: '#3d3d3b',
+  persimmon100: '#a84b2f',
+  red100: '#b4413c',
+  rose100: '#ff5459',
+  teal100: '#20b8cd',
+  teal200: '#21808d'
+}
 
 export const Colors = {
-  light: {
-    text: '#13343b',
-    textSecondary: '#686263',
-    background: '#fcfcf9',
-    backgroundSecondary: '#d8d8cf80',
-    tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
-    tabIconSelected: tintColorLight
-  },
+  black: '#000',
+  gray: '#242026',
+  greyLight: '#B8B3BA',
+  white: '#fff',
   dark: {
-    text: '#e8e8e6',
-    textSecondary: '#938f90',
-    background: '#1b191a',
-    backgroundSecondary: '#232121',
-    tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
-    tabIconSelected: tintColorDark
+    backgroundColor100: ColorsPalette.paleCyan400,
+    backgroundColor200: ColorsPalette.paleCyan300,
+    backgroundColor300: ColorsPalette.paleCyan200,
+    backgroundSuperColor100: ColorsPalette.paleBlue200,
+    backgroundSuperColor200: ColorsPalette.teal100,
+    backgroundSuperColor300: ColorsPalette.teal200,
+    backgroundSuperAltColor100: ColorsPalette.red100,
+    textColor100: ColorsPalette.paleYellow400,
+    textColor200: ColorsPalette.paleCyan100
+  },
+  light: {
+    backgroundColor100: ColorsPalette.paleYellow100,
+    backgroundColor200: ColorsPalette.paleYellow200,
+    backgroundColor300: ColorsPalette.paleYellow300,
+    backgroundSuperColor100: ColorsPalette.paleTeal100,
+    backgroundSuperColor200: ColorsPalette.teal200,
+    backgroundSuperColor300: ColorsPalette.teal100,
+    backgroundSuperAltColor100: ColorsPalette.persimmon100,
+    textColor100: ColorsPalette.paleBlue200,
+    textColor200: ColorsPalette.paleYellow700
   }
 }
