d13728e993aa0b57f8c43216ccd057e55970011a
David AfterDark <david.notjustdev@gmail.com>
2024-08-08 14:10:20 -0400

    feat: init Clerk

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,26 +1,68 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native'
 import { useFonts } from 'expo-font'
-import { Stack, useRouter } from 'expo-router'
+import { Slot, Stack, useRouter, useSegments } from 'expo-router'
 import * as SplashScreen from 'expo-splash-screen'
 import { useEffect } from 'react'
-import 'react-native-reanimated'
-
+import { ClerkProvider, useAuth } from '@clerk/clerk-expo'
 import { useColorScheme } from '@/hooks/useColorScheme'
 import { TouchableOpacity } from 'react-native'
 import { Ionicons } from '@expo/vector-icons'
 import { Colors } from '@/constants/Colors'
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
+import * as SecureStore from 'expo-secure-store'
+
+const CLERK_PUBLISHABLE_KEY = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY ?? ''
+
+export interface TokenCache {
+  getToken: (key: string) => Promise<string | undefined | null>
+  saveToken: (key: string, token: string) => Promise<void>
+  clearToken?: (key: string) => void
+}
+
+const tokenCache = {
+  async getToken(key: string) {
+    try {
+      const item = await SecureStore.getItemAsync(key)
+      if (item !== null) {
+        console.log(`${key} was used 🔐 \n`)
+      } else {
+        console.log('No values stored under key: ' + key)
+      }
+      return item
+    } catch (error) {
+      console.error('SecureStore get item error: ', error)
+      await SecureStore.deleteItemAsync(key)
+      return null
+    }
+  },
+  async saveToken(key: string, value: string) {
+    try {
+      await SecureStore.setItemAsync(key, value)
+    } catch (err) {
+      console.log('[error token cache]: ', err)
+    }
+  }
+}
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync()
 
-export default function RootLayout() {
+const InitialLayout = () => {
   const colorScheme = useColorScheme()
 
   const router = useRouter()
 
-  const [loaded] = useFonts({
+  const segments = useSegments()
+
+  const { isLoaded, isSignedIn } = useAuth()
+
+  const [loaded, error] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf')
   })
+
+  useEffect(() => {
+    if (error !== null) throw error
+  }, [error])
 
   useEffect(() => {
     if (loaded) {
@@ -28,8 +70,22 @@ export default function RootLayout() {
     }
   }, [loaded])
 
-  if (!loaded) {
-    return null
+  useEffect(() => {
+    if (!isLoaded) return
+
+    const segment = segments[0]
+
+    console.log('[segment]: ', segment)
+
+  //   if (isSignedIn && !inAuthGroup) {
+  //     router.replace('/(auth)/(drawer)/(chat)/new')
+  //   } else if (!isSignedIn) {
+  //     router.replace('/')
+  //   }
+  }, [isSignedIn])
+
+  if (!loaded || !isLoaded) {
+    return <Slot />
   }
 
   return (
@@ -62,5 +118,15 @@ export default function RootLayout() {
         <Stack.Screen name="+not-found" />
       </Stack>
     </ThemeProvider>
+  )
+}
+
+export default function RootLayout() {
+  return (
+    <ClerkProvider publishableKey={CLERK_PUBLISHABLE_KEY} tokenCache={tokenCache}>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <InitialLayout />
+      </GestureHandlerRootView>
+    </ClerkProvider>
   )
 }
