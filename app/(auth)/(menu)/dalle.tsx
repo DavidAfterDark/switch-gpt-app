9106a46be11a02ad86d007b76d8537939516d88e
David AfterDark <david.notjustdev@gmail.com>
2024-08-17 10:08:17 -0400

    chore: init view and layouts

---
+++ app/(auth)/(menu)/dalle.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function dalle() {
+  return (
+    <View>
+      <Text>dalle</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
